cce1b706f32e6915f2fb31eb608101ea48798729
Dmitianna <65859176+Dmitianna@users.noreply.github.com>
2024-01-16 14:59:19 +0700

    Первая рабочая версия
    Рабочая версия: Логика сохранилась, немного переписана сама программа, зато теперь реализована функция работы в двух режимах в том виде, в котором его вижу я(т.е. для декодирования используется то же приложение, что и для кодирования)*
    1-Кодирование
    2-Декодирование
    
    Коды символов сохраняются в файле HuffmanCodes.py 
    Путь к файлам указывается без каких-либо кавычек
    Использование функции из os сделан для большего удобства пользователя, чтобы не приходилось долго искать файлы с декодированным текстом
    *Если коды будут сохранены пользователем в HuffmanCodes.py пользователем самостоятельно(в подходящем формате!), то режим декодирования сработает и без применения перед этим кодирования с помощью этой программы, но это все без гарантии правильного выполнения :)

--- HuffMan_Coding.py
+++ HuffMan_Coding.py
@@ -1,111 +1,96 @@
+# coding: utf-8
 import heapq
-from heapq import heappush, heappop
-import ast 
-class Node:
-    def __init__(self, ch, freq, left=None, right=None):
-        self.ch = ch
-        self.freq = freq
-        self.left = left
-        self.right = right
-    #меьшая частотность -> приоритет обработки 
-    def __lt__(self, other):
-          return self.freq < other.freq
+from os.path import abspath 
 
+def build_Huffman_tree(freq):
+    tree = [[weight, [symbol, ""]] for symbol, weight in freq.items()]
+    heapq.heapify(tree)
+                   
+    while len(tree) > 1:
+        son_1 = heapq.heappop(tree)
+        son_2 = heapq.heappop(tree)
+                
+        for code_inf in son_1[1:]:
+                code_inf[1]='0' + code_inf[1]
+                
+        for code_inf in son_2[1:]:
+                code_inf[1]='1' + code_inf[1]
+        total=son_1[0]+ son_2[0]
+        heapq.heappush(tree, [total] + son_1[1:] + son_2[1:])
+        root= tree[0] 
+    return root
 
-def buildHuffmanTree(input_text):
-    # Базовый случай: пустая строка
-    if len(input_text) == 0:
-        return('ce la vie, тут работать не с чем :()')
-    #Создаём словарь частотности встречи каждого символа
-    freq = {}
-    #Заполняем  
-    for ch in input_text: 
-        if ch in freq:
-            freq[ch]+=1
-        else: 
-            freq[ch]=1    
-    #Отсортируем по частотности от самого редкого к самому частому 
-    freq=dict(sorted(freq.items(), key=lambda item: item[1]))
-    print(freq)
-    #Для удобства работы преобразуем в список  
-    freq = [Node(fr,ch) for fr,ch in freq.items()]
-    #преобразовываем данные в структуру кучи
-    heapq.heapify(freq)
-    #пока не получим один узел
-    while len(freq) != 1:
-        #Удаляем два узла с наименьшей частотой
-        left = heappop(freq)
-        right = heappop(freq)
-        # создаем узел с суммарным значением удаленных частот и вставляем в кучу,чтобы структура не нарушилась
-        total = left.freq + right.freq
-        heappush(freq, Node(None, total, left, right))
-    # root - корень
-    root = freq[0]
-    # проходит по дереву Хаффмана и сохраняет коды Хаффмана в словаре.
-    huffmanCode = {}
-    encode(root,'', huffmanCode)
-    print(huffmanCode)
-    with open('HuffmanCodes.txt','w+',encoding='utf-8') as out:
-        for key,val in huffmanCode.items():
-            out.write('{}:{}\n'.format(key,val))
-    return huffmanCode
+def create_huffman_codes(huffman_tree):
+    huffman_codes = {}
+    for code_inf in huffman_tree[1:]:
+        ch , code = code_inf
+        huffman_codes[ch]= code
+    with open('HuffmanCodes.py', 'w+', encoding='UTF-8') as f:
+        f.write('SavedHuffmanCodes = '+str(huffman_codes))
+    return huffman_codes
 
-
-def islast(item):
-    return (item.left is None and item.right is None)
-
-def encode(root, s, huffman_code):
-    if root is None:
-        return("Дерево не построено")
-    # обнаружил листовой узел
-    if islast(root):
-        if len(s) > 0:
-            huffman_code[root.ch] = s;
-        else:
-            huffman_code[root.ch] = '1';
-    encode(root.left, s + '0', huffman_code)
-    encode(root.right, s + '1', huffman_code)
-
-def decode(root, index, s):
-    if root is None:
-        return index
-    # обнаружил листовой узел
-    if islast(root):
-        print(root.ch, end='')
-        return index
-    index = index + 1
-    if s[index] == '0':
-        root = root.left  
-    else:
-        root.right
-    return decode(root, index, s)   
-
-def encoding_text(huffmanCode, input_text):
-    encoded = ''
+def encoding(input_text , huffman_codes):
+    encoded_text = ""
     for ch in input_text:
-        encoded += huffmanCode.get(ch)
-    print('The encoded string is:', encoded)
+        encoded_text += huffman_codes[ch]
+    return encoded_text
 
+def decoding(encoded_text, huffman_codes):
+    decoded_text = ""
 
-def decoding_text(huffmanCode, input_text,s):
-    print('The decoded string is:', end=' ')
-    index = -1
-    while index < len(s) - 1:
-        index = decode(root, index, s)
+    huffman_decodes= {}
+    for ch , code in huffman_codes.items():
+        huffman_decodes[code]= ch
+        
+    temp = ""
+    for binary_digit in encoded_text:
+        temp += binary_digit
+        if temp in huffman_decodes:
+            decoded_text+= huffman_decodes[temp]
+            temp = ""
+            
+    return decoded_text
 
-
-if __name__ == '__main__':
+def main():
+    
     #Получаем на вход текстовый файл. Пока не знаем для кодирования или декодирования
-    try:
-        txt = input("Введите полный путь к файлу, с которым хотите работать: ")
-        file = open(txt,encoding='utf-8',mode= "r")
-        input_text= file.read()
-    except FileNotFoundError:
-        print("Ошибка: Файл не найден")
-    print(input_text)
-    mode= int(input(" В каком режиме запустить программу? 1-закодировать , 2 - декодировать"))
-if mode==1:
-    encoding_text(buildHuffmanTree(input_text),input_text)
-if mode==2:
-    decoding_text(buildHuffmanTree(input_text), input_text)
-file.close()
+    mode = int(input("В каком режиме запустить программу? 1- кодирование, 2 - декодирование"))
+    if(mode==1):
+        try:
+            txt = input("Введите полный путь к файлу, с которым хотите работать: ")
+            file = open(txt,encoding='utf-8',mode= "r")
+            input_text= file.read()
+        except FileNotFoundError:
+            print("Ошибка: Файл не найден")  
+        #Создаём массив частотности встречи каждого символа
+        file.close()
+        frequency = {}
+        #Заполняем этот массив 
+        for i in input_text: 
+            if i in frequency:
+                frequency[i]+=1
+            else: 
+                frequency[i]=1 
+        huffman_tree = build_Huffman_tree(frequency)
+        huffman_codes = create_huffman_codes(huffman_tree)
+        encoded_text = encoding(input_text, huffman_codes)
+        with open('encoded_text.txt', 'w+', encoding='UTF-8') as out:
+            out.write(encoded_text) 
+            print("Закодированный текст находится в файле: ", abspath("encoded_text.txt"))
+        
+    if(mode==2):
+        with open('encoded_text.txt', 'r',encoding='UTF-8') as inp:
+            input_text=inp.read()
+        from HuffmanCodes import SavedHuffmanCodes
+        decoded_text = decoding(input_text, SavedHuffmanCodes)
+        with open('decoded_text.txt', 'w+') as out:
+            out.write(decoded_text)
+            print("Декодированный текст находится в файле: ", abspath("decoded_text.txt"))
+
+main()
+
+
+
+
+
+
